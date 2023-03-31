14d26ada29023e7c22a11e99833139d2cba4d5e5
Mikolaj Klaman <mklaman@virtuslab.com>
2023-03-31 14:04:26 +0200

    feat: static hosting of client dist

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import { staticPlugin } from "@elysiajs/static";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(9000);
+const app = new Elysia()
+  .use(staticPlugin({ assets: "../client/dist", prefix: "" }))
+  .get("/", () => Bun.file("../client/dist/index.html"))
+  .listen(9000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
